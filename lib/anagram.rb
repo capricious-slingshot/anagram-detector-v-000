68a851949324c18cd610a0232892a1a011db293c
Carden Z <ephemere@protonmail.com>
2019-03-26 20:55:47 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Anagram
 	attr_reader :word
 
@@ -9,7 +10,7 @@ class Anagram
     arr.select do |w|
 			if w.length == self.word.length
 	      x = w.split('').collect{|l| self.word.include?(l)}
-				p x
+binding.pry
 				return w if x.length == self.word.length
 			end
 	  end
