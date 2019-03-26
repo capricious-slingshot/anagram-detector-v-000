1a1ad9f9544c7f2745d0e63849cae1c88d12e790
Carden Z <ephemere@protonmail.com>
2019-03-26 20:53:36 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
     arr.select do |w|
 			if w.length == self.word.length
 	      x = w.split('').collect{|l| self.word.include?(l)}
-				return x if x.length == self.word.length
+				return w if x.length == self.word.length
 			end
 	  end
   end
